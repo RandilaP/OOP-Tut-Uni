7449598ce1c30315e20cf99e2888023230c3d6d8
Randila Premarathne <randilamenukapremarathne@gmail.com>
2024-01-10 12:19:49 +0530

    Add new constructor and coors

--- week1/TutorialCircle/Circle.java
+++ week1/TutorialCircle/Circle.java
@@ -3,18 +3,28 @@ public class Circle {
     private String color;
 
     public Circle(){
-        radius = 1.0;
-        color = "Blue";
+        this.radius = 1.0;
+        this.color = "Blue";
     };
     public Circle(double radius){
-        radius = radius;
-        color = "Blue";
+        this.radius = radius;
+        this.color = "Blue";
     }
+
+    public Circle(double radius, String color){
+        this.radius = radius;
+        this.color = color;
+    }
+
     public double getRadius(){
         return radius;
     }
 
     public double getArea(){
         return Math.PI * radius * radius;
+    }
+
+    public String getColor(){
+        return color;
     }
 }
--- week1/TutorialCircle/TutorialClass.java
+++ week1/TutorialCircle/TutorialClass.java
@@ -3,9 +3,16 @@ public class TutorialClass {
         Circle circle1 = new Circle();
         System.out.println("Radius of circle1 is " + circle1.getRadius());
         System.out.println("Area of circle1 is " + circle1.getArea());
-        Circle circle2 = new Circle(8);
+        System.out.println("Color of circle1 is " + circle1.getColor());
 
+        Circle circle2 = new Circle(8);
         System.out.println("Radius of circle2 is " + circle2.getRadius());
         System.out.println("Area of circle2 is " + circle2.getArea());
+        System.out.println("Color of circle2 is " + circle2.getColor());
+
+        Circle circle3 = new Circle(5, "Red");
+        System.out.println("Radius of circle3 is " + circle3.getRadius());
+        System.out.println("Area of circle3 is " + circle3.getArea());
+        System.out.println("Color of circle3 is " + circle3.getColor());
     }
 }
